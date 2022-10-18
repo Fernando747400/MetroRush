af7fa533ac8e688a15b9d2624ae7ed0c9c2ed558
Rojo5Amerike <rojo5@amerike.edu.mx>
2022-10-18 18:09:00 -0500

    Store changes

--- MetroRushGame/Assets/_Code/UI/StoreView.cs
+++ MetroRushGame/Assets/_Code/UI/StoreView.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class StoreView : MonoBehaviour
 {
+    [Header("Dependencies")]
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _basePosition;
+    [SerializeField] private Canvas _baseCanvas;
+
     [Header("Motores")]
     [SerializeField] private GameObject _motorsPosition;
     [SerializeField] private Canvas _motorCanvas;
@@ -24,4 +29,36 @@ public class StoreView : MonoBehaviour
     [SerializeField] private GameObject _acPosition;
     [SerializeField] private Canvas _acCanvas;
 
+    public void ChangeCanvasAndCamera(string name)
+    {
+
+    }
+
+    private void ChangeTo(GameObject positionObject)
+    {
+        iTween.MoveTo(_camera.gameObject, positionObject.transform.position, 2f);
+        iTween.RotateTo(_camera.gameObject, positionObject.transform.eulerAngles, 2f);
+    }
+
+    private void ActivateCanvas(Canvas activeCanvas)
+    {
+        CloseAllCanvas();
+        activeCanvas.gameObject.SetActive(true);
+    }
+
+    private void CloseAllCanvas()
+    {
+        _motorCanvas.gameObject.SetActive(false);
+        _chasisCanvas.gameObject.SetActive(false);
+        _breakCanvas.gameObject.SetActive(false);
+        _capacityCanvas.gameObject.SetActive(false);
+        _acCanvas.gameObject.SetActive(false);
+    }
+}
+
+public struct StoreValues
+{
+    public GameObject PositionObject;
+    public Canvas CanvasObject;
+    public string Description;
 }
